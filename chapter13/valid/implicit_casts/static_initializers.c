456b263bc8ea9501eebaf0d43e0228d5c1c3d05f
Nora Sandler <nlsandler@gmail.com>
2022-06-01 20:53:13 -0700

    remove static initializer we do not support

--- chapter13/valid/implicit_casts/static_initializers.c
+++ chapter13/valid/implicit_casts/static_initializers.c
@@ -4,14 +4,15 @@
 
 /* This should be initialized to 4294967246,
  * or 2^32 - 50
+ * NOTE: can't handle constant expressions, only constants
  */
-unsigned int ui = -50;
+// unsigned int ui = -50;
 
 /* This should be initialized to 18446744073709551566,
  * or 2^64 - 50
  */
 
-unsigned long ul = -50;
+// unsigned long ul = -50;
 
 /* This should be initialized to -2147483646,
  * or 2147483650 - 2^32
@@ -23,11 +24,14 @@ int i = 2147483650;
  */
 long l = 9223372036854775900u;
 
-int main() {
+int main()
+{
+    /*
     if (ui != 4294967246u)
         return 0;
     if (ul != 18446744073709551566ul)
         return 0;
+    */
     if (i != -2147483646)
         return 0;
     if (l != -9223372036854775716l)
