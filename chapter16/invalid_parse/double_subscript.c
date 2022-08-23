076a30755b6bb9020e7b4055d3eb4cf23df3f919
Nora Sandler <nlsandler@gmail.com>
2022-08-22 17:04:13 -0700

    non-integer subscript is type error, not parse error

--- chapter16/invalid_parse/double_subscript.c
+++
@@ -1,6 +0,0 @@
-/* NOTE: could be a type error depending on how you implement this */
-int main()
-{
-    int arr[3] = {4, 5, 6};
-    return arr[2.0];
-}
---
+++ chapter16/invalid_types/double_subscript.c
@@ -0,0 +1,5 @@
+int main()
+{
+    int arr[3] = {4, 5, 6};
+    return arr[2.0];
+}
