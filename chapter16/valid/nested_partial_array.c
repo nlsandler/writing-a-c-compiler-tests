0bc16dfc68747bb0706530a57f10a52876a01dc8
Nora Sandler <nlsandler@gmail.com>
2022-08-22 17:00:46 -0700

    do not use char in chapter 16 tests

--- chapter16/valid/nested_partial_array.c
+++ chapter16/valid/nested_partial_array.c
@@ -1,8 +1,7 @@
-// #include <stdio.h>
 
 int main()
 {
-    char arr[3][2][5] = {
+    int arr[3][2][5] = {
         {{1, 2},
          {3, 4, 5}},
         {{6}},
@@ -12,13 +11,6 @@ int main()
     int sum = arr[0][0][0] + arr[0][0][1] + arr[0][1][0] + arr[0][1][1] + arr[0][1][2] + arr[1][0][0] + arr[2][0][0];
     for (int i = 0; i < 5; i = i + 1)
         sum = sum + arr[2][1][i];
-
-    /*
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 2; j++)
-            for (int k = 0; k < 5; k++)
-                printf("arr[%d][%d][%d]: %d\n", i, j, k, arr[i][j][k]);
-    */
 
     return sum;
 }
