8f5e75986d2d89bcc938ef8520dea9734d31c195
Nora Sandler <nlsandler@gmail.com>
2022-08-30 12:43:38 -0700

    better test coverage of static initializers

--- chapter13/valid/implicit_casts/static_initializers.c
+++ chapter13/valid/implicit_casts/static_initializers.c
@@ -2,40 +2,55 @@
  * implicitly-converted value at program startup
  */
 
-/* This should be initialized to 4294967246,
- * or 2^32 - 50
- * NOTE: can't handle constant expressions, only constants
- */
-// unsigned int ui = -50;
-
-/* This should be initialized to 18446744073709551566,
- * or 2^64 - 50
- */
-
-// unsigned long ul = -50;
+// this is 2^60 + 2^31 + 12
+// should be truncated to 2^31 + 12 (which is 2147483660)
+unsigned int u = 1152921506754330636l;
 
 /* This should be initialized to -2147483646,
  * or 2147483650 - 2^32
  */
-int i = 2147483650;
+int i = 2147483650u;
 
 /* This should be initialized to -9223372036854775716,
  * or 9223372036854775900 - 2^64
  */
-long l = 9223372036854775900u;
+long l = 9223372036854775900u; // note: this has type unsigned long
+
+// this can be converted to a long with no change in value
+long l2 = 2147483650u;
+
+// any unsigned int can be converted to an unsigned long w/ no change in value
+unsigned long ul = 4294967294u;
+
+/* any signed long _literal_ can be converted to an unsigned long w/ no change in value
+ * (we don't support negation expressions in constant initializers) */
+unsigned long ul2 = 9223372036854775798l;
+
+// truncate ulong 2**63 + 2**31 + 150
+// to int -2**31 + 150 (which is -2147483498)
+int i2 = 9223372039002259606ul;
+
+// truncate ulong 2**63 + 2**31 + 150
+// to uint 2**31 + 150 (which is 2147483798)
+unsigned ui2 = 9223372039002259606ul;
 
 int main()
 {
-    /*
-    if (ui != 4294967246u)
+    if (u != 2147483660u)
         return 0;
-    if (ul != 18446744073709551566ul)
-        return 0;
-    */
     if (i != -2147483646)
         return 0;
     if (l != -9223372036854775716l)
         return 0;
-
+    if (l2 != 2147483650l)
+        return 0;
+    if (ul != 4294967294ul)
+        return 0;
+    if (i2 != -2147483498)
+        return 0;
+    if (ul2 != 9223372036854775798ul)
+        return 0;
+    if (ui2 != 2147483798u)
+        return 0;
     return 1;
 }
