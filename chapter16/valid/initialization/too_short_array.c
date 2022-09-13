a9dfd69a685bba4ecb944c4d3a318bd918c11bb9
Nora Sandler <nlsandler@gmail.com>
2022-09-13 15:23:08 -0700

    better test coverage

--- chapter16/valid/initialization/too_short_array.c
+++ chapter16/valid/initialization/too_short_array.c
@@ -1,8 +1,9 @@
-int main()
-{
-    /* if some elments are explicitly initialized, remaining elements
-     * should be initialized to zero
-     */
-    int arr[3] = {1};
-    return arr[0];
+int main() {
+  /* if some elments are explicitly initialized, remaining elements
+   * should be initialized to zero
+   */
+  int arr[3] = {1};
+  unsigned int arr2[2] = {3};
+  return arr[0] == 1 && arr[1] == 0 && arr[2] == 0 && arr2[0] == 3 &&
+         arr2[1] == 0;
 }
