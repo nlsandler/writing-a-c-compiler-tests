50f1f1b554a48e0682cb128360e185b5b7af032d
Nora Sandler <nlsandler@gmail.com>
2022-12-13 20:55:14 -0800

    test coverage of cvttsd2il rewrite rule

---
+++ chapter14/valid/explicit_casts/double_to_int.c
@@ -0,0 +1,15 @@
+double glob = 3.0;
+
+int main() {
+
+    // this is a regression test for a bug I found in my reference implementaton,
+    // instruction fix-up rewrite cvttsd2sil -8(%rbp), -12(%rbp) to
+    // cvttsdi2sil -8(%rbp), %r11d
+    // movq        %r11, %-12(%rbp)
+    // which potentilly clobbers other things on the stack
+    // we include other variables on the stack in order to catch this
+    int i = 10;
+    int j = (int) glob;
+    int k = 20;
+    return i + j + k;
+}
